0016f6af2d85699f32bcca1a9699a0ba3f1e5538
徐志兴 <xuzhix@szlanyou.com>
2026-02-09 10:46:05 +0800

    npm打包包含wasm

---
+++ pkg/wasm_watermark_bg.wasm.d.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_watermarkerror_free: (a: number, b: number) => void;
+export const add_watermark: (a: number, b: number, c: number, d: number) => void;
+export const add_watermark_async: (a: number, b: number, c: number) => number;
+export const init: () => void;
+export const watermarkerror_message: (a: number, b: number) => void;
+export const __wasm_bindgen_func_elem_364: (a: number, b: number) => void;
+export const __wasm_bindgen_func_elem_473: (a: number, b: number, c: number, d: number) => void;
+export const __wasm_bindgen_func_elem_370: (a: number, b: number, c: number) => void;
+export const __wbindgen_export: (a: number, b: number) => number;
+export const __wbindgen_export2: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export3: (a: number) => void;
+export const __wbindgen_export4: (a: number, b: number, c: number) => void;
+export const __wbindgen_add_to_stack_pointer: (a: number) => number;
+export const __wbindgen_start: () => void;
